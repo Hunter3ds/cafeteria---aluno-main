22cdb352f090c74178e3625a5554b42b581973ed
Hunter3ds <ghostmat817@gmail.com>
2023-11-22 16:41:46 -0300

    Cafeteria react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/Banner";
+import Formulario from "./components/Formulario/Formulario";
+
+function App(){
+    return(         
+        // <Formulario/>
+        <div className='App'>
+            <Banner/>
+            <Formulario/>
+        </div>
+    )
+}
+export default App;
---
+++ src/components/Formulario/Formulario.js
@@ -0,0 +1,12 @@
+import CampoTexto from "../CampoTexto/CampoTexto";
+
+const Formulario = ()=>{
+    <section>
+        <form>
+            <CampoTexto label='Nome' placeholder={'digite seu nome'}/>
+            <CampoTexto label='E-mail' placeholder={'digite seu E-mail'}/>
+            <CampoTexto label='Senha' placeholder={'digite sua senha'}/>
+        </form>
+    </section>
+}
+export default Formulario
